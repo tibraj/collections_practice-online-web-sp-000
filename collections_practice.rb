24587612fb1787e8b4e6cebe7f39db9371160766
Taulant Ibraj <tibraj94@gmail.com>
2020-03-31 19:32:18 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,47 @@
+def sort_array_asc(array)
+  array.sort
+  end
 
+def sort_array_desc(array)
+  array.sort.reverse
+end
+
+
+def sort_array_char_count(array)
+  array.sort do |a, b|
+    a.length <=> b.length
+  end
+end
+
+def swap_elements(array)
+  array[1..2] = array[2], array[1]
+  return array
+end
+
+def reverse_array(array)
+  return array.reverse
+end
+
+def kesha_maker(array)
+  array.each do |element|
+    element[2] = "$"
+  end
+end
+
+def find_a(array)
+  array.select {|element| element[0] == "a"}
+end
+
+def sum_array(array)
+  array.inject(0) { |result, element| result + element}
+end
+
+def add_s(array)
+  array.collect do |element|
+    if array[1] == element
+      element
+    else
+      element + "s"
+    end
+  end
+end
